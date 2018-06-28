9e918b239dd3b40118fdbd2a853128eb359a7e73
Danimar Veriato <danimarveriato@gmail.com>
2018-06-27 23:54:49 -0300

    [COMMIT] - Alteração Retiro de Inverno 2018

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "March 03, 2018 08:00:00",
+	date: "July 07, 2018 09:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
